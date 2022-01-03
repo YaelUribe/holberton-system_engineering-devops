b0e4b793d8551c17d15483eec41b8f84e8e32505
YaelUribe <yael3x@gmail.com>
2022-01-03 17:29:33 -0500

    removed shelln

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Puppet manifest to execute a command
 
 exec { 'killmenow':
-    command  => 'pkill -f killmenow',
-    path     => '/usr/bin/:/bin/:/usr/local/bin/',
+    command => 'pkill -f killmenow',
+    path    => '/usr/bin/:/bin/:/usr/local/bin/',
 }
