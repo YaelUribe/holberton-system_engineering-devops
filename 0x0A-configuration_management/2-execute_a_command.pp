49a51e86a6778413185f38c66ccff0351a20e88e
YaelUribe <yael3x@gmail.com>
2022-01-03 17:25:43 -0500

    corrected path

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # Puppet manifest to execute a command
 
 exec { 'killmenow':
-    path     => '/usr/bin:/bin/',
+    path     => '/usr/bin/:/bin/:/usr/local/bin/',
     command  => 'pkill killmenow',
     resource => 'shell',
 }
