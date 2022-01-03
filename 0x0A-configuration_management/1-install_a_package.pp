20d08e8c537f94c486b88c52418adc004713b6cf
YaelUribe <yael3x@gmail.com>
2022-01-03 18:45:44 -0500

    returned to version 2.5.0, and added source

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # puppet manifest to install a package Puppet-lint
 
 package { 'puppet-lint':
-    ensure   => '<=2.5.2',
+    ensure   => '2.5.0',
     provider => 'gem',
+    source   => 'https://rubygems.org',
 }
