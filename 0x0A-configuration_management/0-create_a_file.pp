ab4d530da62e2f05019170c417e140fb78046e28
YaelUribe <yael3x@gmail.com>
2022-01-03 16:28:32 -0500

    :cry:

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
-# puppet manifest to create a file
+# Puppet manifest to create a file
 
-file { 'school':
+file { '/tmp/school':
     ensure  => file,
     path    => '/tmp/school',
     mode    => '0744',
