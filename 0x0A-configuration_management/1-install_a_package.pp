b70461d08ff35d10317bce57d2bc6c8e085fedbc
YaelUribe <yael3x@gmail.com>
2022-01-03 17:35:57 -0500

    changed to an older version

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # puppet manifest to install a package Puppet-lint
 
-package { 'sudo puppet-lint':
-    ensure   => '2.5.0',
+package { 'puppet-lint':
+    ensure   => '2.1.1',
     provider => 'gem',
 }
