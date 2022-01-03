5356fed20536584473f26e0757622927c723890c
YaelUribe <yael3x@gmail.com>
2022-01-03 15:54:44 -0500

    changed lines order

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,7 +2,7 @@
 file { 'school':
     path    => '/tmp/school',
     mode    => '0744',
-    content => 'I love puppet',
     owner   => 'www-data',
     group   => 'www-data',
+    content => 'I love puppet',
 }
