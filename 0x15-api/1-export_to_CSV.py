5e10b1eda82256399f99ac8e86805b68b8b251ce
YaelUribe <yael3x@hotmail.com>
2022-01-31 17:27:32 -0500

    Task 1, storing data into CSV file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Python script for gathering data from an API"""
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    EMPLOYEE_ID = argv[1]
+    URL = 'https://jsonplaceholder.typicode.com/'
+    USER = requests.get('{}users/{}'.format(URL, EMPLOYEE_ID)).json()
+    TODO = requests.get('{}todos?userId={}'.format(URL, EMPLOYEE_ID)).json()
+
+    #  export data to CSV
+    USER_ID = "{}.csv".format(EMPLOYEE_ID)
+    with open(USER_ID, "w")as f:
+        FILE = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for row in TODO:
+            FILE.writerow(USER['id'],
+                          USER['username'],
+                          row['completed'],
+                          row['title'])
